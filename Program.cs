b5406d1d7638e1b7734947c9f9d7abc21241cb39
Ivan <gerano.03@mail.ru>
2023-12-25 22:01:31 +0300

    первое задание

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+namespace program;
+    class program
+    {
+    static void Main()
+    {
+    char[,] mass = new char[1,33]; // Задаём двумерный массив, состоящий из 1 строки и 33 столбцов
+     for (int i = 0; i < mass.GetLength(0); i++) //цикл перебора строк
+     {
+        for (int j = 0; j < mass.GetLength(1); j++) // цикл перебора столбцов 
+      {
+        mass[i,j] = Convert.ToChar(new Random().Next(41,59)); //генерация рандомных значений юникода и конвертация их в тип char
+        Console.Write($"{mass[i,j]} ");  //вывод массива 
+      }
+      Console.WriteLine();
+     }
+    }
+}
